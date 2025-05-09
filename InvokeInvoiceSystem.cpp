fd119ab889649e3cbc5f712f9733febe8c12ba62
Fabian Di Mento <fdimento33@gmail.com>
2025-05-09 23:42:15 +1000

    Initial Commit
    Commit all source code from VS

---
+++ InvokeInvoiceSystem.cpp
@@ -0,0 +1,12 @@
+﻿// InvokeInvoiceSystem.cpp : Defines the entry point for the application.
+//
+
+#include "InvokeInvoiceSystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
