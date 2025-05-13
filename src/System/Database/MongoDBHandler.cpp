d708a2bde7ec40b05f2723a0a259ac5b8b8d559b
Fabian <fdimento33@gmail.com>
2025-05-13 10:09:42 +1000

    Test for other URI

--- src/System/Database/MongoDBHandler.cpp
+++ src/System/Database/MongoDBHandler.cpp
@@ -4,7 +4,7 @@
 mongocxx::instance MongoDBHandler::instance{};
 
 MongoDBHandler::MongoDBHandler()
-	: client{ mongocxx::uri{ std::getenv("INVOICE_DB_URI") } } {
+	: client{ mongocxx::uri{ std::getenv("INVOICE_DB_URI") } } { //try MONGODB_URI
 }
 
 mongocxx::database MongoDBHandler::getDatabase() const {
