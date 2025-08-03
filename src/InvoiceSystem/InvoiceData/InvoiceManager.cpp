3fe86c653e6a983d3403c424694b2395f306551c
Fabian <fdimento33@gmail.com>
2025-08-03 22:06:39 +1000

    almost working

---
+++ src/InvoiceSystem/InvoiceData/InvoiceManager.cpp
@@ -0,0 +1,18 @@
+#include "InvoiceSystem/InvoiceManager.h"
+
+std::shared_ptr<Invoice> InvoiceManager::currentInvoice = nullptr;
+
+void InvoiceManager::setCurInvoice(std::shared_ptr<Invoice> invoice) {
+	currentInvoice = invoice;
+}
+
+std::shared_ptr<Invoice> InvoiceManager::getCurInvoice() {
+	return currentInvoice;
+}
+
+//void InvoiceManager::setCurInvoice(const std::string invoiceID) {
+//	SetInvoice setInvoices(dbManager);
+//	setInvoices.setInvoice(invoiceID);
+//}
+
+
